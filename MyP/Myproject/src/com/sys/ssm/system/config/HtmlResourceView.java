6d4ca84a1fc8ae5b3d194ad130fed798295c2c2a
a249853772 <249853772@qq.com>
2018-06-23 10:37:26 +0800

    dd

---
+++ MyP/Myproject/src/com/sys/ssm/system/config/HtmlResourceView.java
@@ -0,0 +1,18 @@
+package com.sys.ssm.system.config;
+
+import java.io.File;
+import java.util.Locale;
+
+import org.springframework.web.servlet.view.InternalResourceView;
+
+public class HtmlResourceView extends InternalResourceView{
+	
+	@Override  
+    public boolean checkResource(Locale locale) {  
+     File file = new File(this.getServletContext().getRealPath("/") + getUrl());  
+     return file.exists();// 判断该页面是否存在  
+    } 
+	
+	
+
+}
